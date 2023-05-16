62a5fe90720c44bf8999e57ef337bf943ea1ff64
nargespoorhatamii <126357653+nargespoorhatamii@users.noreply.github.com>
2023-05-16 16:46:18 +0430

    Add project files.

---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace Tamrin
+{
+    public class Teacher
+    {
+        private String id;
+        private String name;
+        private String lastname;
+        private int hour;
+        private int payperonehour;
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }  
+        }
+        public string Lastname
+        {
+            get { return lastname; }   
+            set { lastname = value; }
+        }
+        public int Hour
+        {
+            get { return hour; }
+            set { hour = value; }
+        }
+        public int Payperonehour
+        {
+            get { return payperonehour; }
+            set { if(value>0)
+                {
+                    payperonehour = value;
+                }
+            }
+        }
+
+        public Teacher() { } //mokharreb function
+        public Teacher(String Id,String Name , String Lastname , int Hour , int Payperonehour)
+        {
+            id= Id;
+            name=Name;
+            lastname= Lastname; 
+            hour= Hour; 
+            payperonehour= Payperonehour;
+
+        }//sazande function
+        private long Payment()
+        {
+            return hour * Payperonehour;
+        }
+      
+    }
+}
---
+++ ConsoleApp26/class teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tamrin
+{
+       public  class Teacher
+    {
+        public static void Main()
+        {
+           Teacher th= new teacher("123","Ali",16,60000);
+            long Pay = th.Payment();
+            Console.WriteLine(Pay);
+        }
+    }
+}
